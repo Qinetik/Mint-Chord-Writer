31bcf1b4389fd1360b3361947e3ead76f6add43e
MintHana <minthana1137@gmail.com>
2021-12-04 00:20:50 -0500

    Added home button to go back to Main

--- app/src/main/java/com/example/mint_chord_writer/EditActivity.kt
+++ app/src/main/java/com/example/mint_chord_writer/EditActivity.kt
@@ -1,11 +1,23 @@
 package com.example.mint_chord_writer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+
+        val mainButton = findViewById<Button>(R.id.editToMain)
+        mainButton.setOnClickListener{
+            finish();
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("destroyed")
     }
 }
